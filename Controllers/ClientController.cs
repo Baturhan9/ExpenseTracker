0c718fe8ba5699054cc6e34f433b87e1b9e3d744
Baturhan <baturxan-06.72@mail.ru>
2023-10-20 14:59:18 +0600

    add repository pattern

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 using ExpenseTracker.Data;
+using ExpenseTracker.Interfaces;
+using ExpenseTracker.Interfaces.ClientInterface;
 using ExpenseTracker.Models;
+using ExpenseTracker.Repositorys;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -14,23 +17,46 @@ namespace ExpenseTracker.Controllers
     public class ClientController : Controller
     {
         private readonly ILogger<ClientController> _logger;
-        private readonly AppExpenseTrackerContext _context; 
+        private readonly IRepository<Client> _repository;
+        private readonly IClientFinder _finder;
         private readonly IHttpContextAccessor _http;
-        public ClientController(ILogger<ClientController> logger, AppExpenseTrackerContext context, IHttpContextAccessor http)
+        public ClientController(ILogger<ClientController> logger,IHttpContextAccessor http)
         {
             _logger = logger;
-            _context = context;
             _http = http;
+            _repository = new ClientRepository();
+            _finder = new ClientRepository();
         }
 
-        public IActionResult Index(Client client)
+        public IActionResult Index()
         {
-            _http.HttpContext.Session.Set<Client>("Client", client);
-
-            ViewData["Name"] = _http.HttpContext.Session.Get<Client>("Client").CName;
             return View();
         }
+        [HttpPost] 
+        public IActionResult Index(string clientLogin, string clientPassword)
+        {
+            if(isEmptyField(clientLogin, clientPassword))
+            {
+                ViewBag.EmptyFields = "missing fields";
+                return View();
+            }
+            var client = _finder.FindClient(clientLogin, clientPassword);
+            if(client == null)
+            {
+                ViewBag.NotFound = "no user was found";
+                return View();
+            }
+            return RedirectToAction("ClientMenu");
+        }
+        public IActionResult ClientMenu() => View();
 
+        
+        private bool isEmptyField(string s1, string s2)
+        {
+            if(s1 is null || s2 is null)
+                return true;
+           return false; 
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Repositorys/ClientRepository.cs
@@ -0,0 +1,71 @@
+using ExpenseTracker.Interfaces;
+using ExpenseTracker.Interfaces.ClientInterface;
+using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Repositorys;
+
+public class ClientRepository : IRepository<Client>, IClientFinder
+{
+    private AppExpenseTrackerContext db;
+    public ClientRepository()
+    {
+        db = new AppExpenseTrackerContext();
+    }
+    public void Create(Client item)
+    {
+        db.Clients.Add(item);
+    }
+
+    public void Delete(int id)
+    {
+        var client = db.Clients.Find(id);
+        if(client != null)
+            db.Clients.Remove(client);
+    }
+
+    private bool disposed = false;
+ 
+    public virtual void Dispose(bool disposing)
+    {
+        if(!this.disposed)
+        {
+            if(disposing)
+            {
+                db.Dispose();
+            }
+        }
+        this.disposed = true;
+    }
+
+    public List<Client>? GetAll()
+    {
+        return db.Clients.ToList();
+    }
+
+    public Client? GetOne(int id)
+    {
+        return db.Clients.Find(id);
+    }
+
+    public void Save()
+    {
+        db.SaveChanges();
+    }
+
+    public void Update(Client item)
+    {
+        db.Entry(item).State = EntityState.Modified;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    public Client? FindClient(string loing, string password)
+    {
+        return db.Clients.FirstOrDefault(c => c.CLogin == loing && c.CPassword == password);
+    }
+}

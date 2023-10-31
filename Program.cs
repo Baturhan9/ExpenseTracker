cb6c8911ee9c0f23cd02f6de77919ee3a4d3a289
Baturhan <baturxan-06.72@mail.ru>
2023-10-31 17:25:07 +0600

    Add russian and english localization

--- Program.cs
+++ Program.cs
@@ -5,11 +5,28 @@ using ExpenseTracker.Middleware;
 using ExpenseTracker.Models;
 using ExpenseTracker.Repositorys;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix);
+
+builder.Services.AddLocalization(option =>
+{
+    option.ResourcesPath = "Resources";
+});
+
+builder.Services.Configure<RequestLocalizationOptions>(option =>
+{
+    var supportedCultures = new[] {"en","ru"};
+    option.SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+});
+
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
@@ -23,7 +40,7 @@ builder.Services.AddDbContext<AppExpenseTrackerContext>(option =>
     option.UseNpgsql("Host=localhost;Port=5432;Database=app_expense_tracker;username=postgres;password=1"));
 
 var app = builder.Build();
-
+app.UseRequestLocalization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
